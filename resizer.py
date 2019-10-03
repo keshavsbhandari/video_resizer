505d2230fd345261fa6384a744499823901ca9e8
keshavsbhandari <keshav.s.bhandari@gmail.com>
2019-10-03 16:04:58 -0500

    resizer added

--- resizer.py
+++ resizer.py
@@ -6,8 +6,11 @@ import progressbar as pb
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--source", help="increase output verbosity", type=str, default='/home/keshav/data/kinetics/train',action="store")
-parser.add_argument("--destination", help="increase output verbosity", type=str,default='kinetics_resized/train/',action="store")
+parser.add_argument("--source", help="source video folder eg: /home/keshav/data/kinetics/train", type=str, default='/home/keshav/data/kinetics/train',action="store")
+parser.add_argument("--destination", help="destination folder eg: /keshav/data/kinetics_resized/train", type=str,default='kinetics_resized/train/',action="store")
+parser.add_argument("--outsize", help="output size of video heightxwidth, input format eg: 256x256", type=str,default='256x256',action="store")
+parser.add_argument("--outrate", help="output frame rate", type=str,default='10',action="store")
+parser.add_argument("--nprocess", help="number of process", type=int,default=2,action="store")
 args = parser.parse_args()
 
 
@@ -22,6 +25,9 @@ dirpath = path.glob('*')
 
 source = args.source
 destination = args.destination
+outsize = args.outsize
+outrate = args.outrate
+nbprocess = args.nbprocess
 
 
 path = Path(source)
@@ -69,29 +75,24 @@ bar = pb.ProgressBar(widgets=wid, maxval=total_size).start()
 
 def process(paths):
     """
-    :param paths:
-    :type paths:
-    :return:
-    :rtype:
+    :param paths: list of pairs of source, destination video path
+    :type paths: list of lists(of size 2, str path)
     """
     global done_list
     in_path, out_path = paths
     try:
         out = subprocess.check_call(['ffmpeg', '-y','-hwaccel', 'cuvid', '-c:v', 'h264_cuvid',
-                     '-resize', '256x256','-y','-i', in_path,
-                     '-c:a', 'copy', '-c:v', 'h264_nvenc','-b:v', '5M','-r', '10','-an',out_path],
+                     '-resize', outsize,'-y','-i', in_path,
+                     '-c:a', 'copy', '-c:v', 'h264_nvenc','-b:v', '5M','-r', outrate,'-an',out_path],
                                           stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         counter.value += 1
         bar.update(counter.value)
-
-        # print(f'{counter.value} of {total_size} {100 * counter.value/50:.2f}% DONE')
     except subprocess.CalledProcessError as e:
         errorlist.append(paths)
         print(f'{out_path} cannot be written out of memory')
         if Path(out_path).exists():Path(out_path).unlink()
     except OSError as e:
         if Path(out_path).exists():Path(out_path).unlink()
-    # '-t', '00:00:10.0'
 
 
 def makedir(x):
@@ -104,20 +105,16 @@ def create_dir(n = 12):
     with Pool(processes=n) as pool:
         pool.map(makedir, dir_list, len(dir_list)//n)
 
-def main(n = 1):
+def main(n = 2):
     create_dir()
     with Pool(processes=n) as pool:
         pool.map(process, remainng, len(remainng)//n)
     print("\nCOMPLETED")
 
 if __name__ == '__main__':
-    main()
+    main(nbprocess)
     print(errorlist)
     json.dump(list(errorlist),open('error.txt','w'))
-
-    # for d,_ in list(errorlist):
-    #     Path(d).unlink()
-
 
 """
 source ~/anaconda3/bin/activate && conda activate attention && cd ~/data/resizer/ && python resizer.py --destination /home/keshav/data/kinetics_resized
